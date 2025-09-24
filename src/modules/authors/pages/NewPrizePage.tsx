4d1b99eab187b459a22574294394411f38c267dc
Angie Gutierrez <ac.gutierrezt1@uniandes.edu.co>
2025-09-24 09:24:15 -0500

    no alcancé premios :(

---
+++ src/modules/authors/pages/NewPrizePage.tsx
@@ -0,0 +1,6 @@
+// crear un nuevoo premio
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+// import PrizeForm from "@/modules/authors/ui/PrizeForm";}
+
